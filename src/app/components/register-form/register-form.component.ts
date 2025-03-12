0b93d732a8c544dc3fd820eba44bac268c50ffa1
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-03-12 03:28:55 -0400

    Add functionality to RegisterFormComponent
    - Removed password field.
    - Implemented form validation.
    - When submitted adds the new user to local storage.

--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -1,11 +1,33 @@
-import { Component, output } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TodoDataService } from '../../services/todo-data.service';
 
 @Component({
   selector: 'app-register-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.scss',
 })
 export class RegisterFormComponent {
   loginLinkClicked = output();
+  formSubmitted = output();
+  tds = inject(TodoDataService);
+
+  signUpObj = {
+    name: '',
+    email: '',
+    acceptedTerms: false,
+  };
+
+  onSubmit() {
+    this.tds.addUser(this.signUpObj.email, this.signUpObj.name);
+
+    this.formSubmitted.emit();
+
+    this.signUpObj = {
+      name: '',
+      email: '',
+      acceptedTerms: false,
+    };
+  }
 }
