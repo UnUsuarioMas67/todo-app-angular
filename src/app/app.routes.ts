61f5d43d8c91c0e4b24620dcccf935986f1a6963
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-03-12 03:32:31 -0400

    Update Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: LoginComponent,
+  },
+  {
+    path: 'todo',
+    component: TodoListComponent,
+  },
+];
