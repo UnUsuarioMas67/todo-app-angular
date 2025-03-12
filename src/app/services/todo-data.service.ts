b361f5173f44d78d450dc3b8a7a59e1009cb1020
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-03-12 02:03:25 -0400

    Create TodoDataService

---
+++ src/app/services/todo-data.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Task, User } from '../model/todo-data.type';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoDataService {
+  localStorageKey = 'todo-users';
+
+  createLocalStorageItem() {
+    const users = [
+      {
+        id: 0,
+        name: 'John Johnson',
+        email: 'jj666@mail.net',
+        tasks: [],
+      },
+    ];
+
+    localStorage.setItem(this.localStorageKey, JSON.stringify(users));
+  }
+
+  setLocalStorageItem(users: Array<User>) {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(users));
+  }
+
+  getAllUsers(): Array<User> {
+    if (!localStorage.getItem(this.localStorageKey)) {
+      this.createLocalStorageItem();
+    }
+
+    let json = localStorage.getItem(this.localStorageKey)!;
+    return JSON.parse(json);
+  }
+
+  getUserByEmail(email: string) {
+    const users = this.getAllUsers();
+    return users.find((u) => u.email === email);
+  }
+
+  getTask(user: User, id: number) {
+    return user.tasks.find((t) => t.id === id);
+  }
+}
