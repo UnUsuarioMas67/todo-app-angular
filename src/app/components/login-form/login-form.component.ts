df00d018a5478fb6ef1ca3f931e98cb1bc43bd64
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-03-12 02:29:03 -0400

    Add functionality to LoginFormComponent
    - Removed password field.
    - Add validation to the form.
    - Outputs an User object.

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,11 +1,36 @@
-import { Component, output } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
+import { FormsModule, FormSubmittedEvent } from '@angular/forms';
+import { User } from '../../model/todo-data.type';
+import { TodoDataService } from '../../services/todo-data.service';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss',
 })
 export class LoginFormComponent {
+  tds = inject(TodoDataService);
   registerLinkClicked = output();
+  formSubmitted = output<User>();
+
+  userNotExists = false;
+  emailValue = '';
+
+  formFilled() {
+    return !!this.emailValue;
+  }
+
+  onSubmit() {
+    const user = this.tds.getUserByEmail(this.emailValue);
+    this.emailValue = '';
+
+    if (!user) {
+      this.userNotExists = true;
+      return;
+    }
+
+    this.formSubmitted.emit(user);
+    this.userNotExists = false;
+  }
 }
