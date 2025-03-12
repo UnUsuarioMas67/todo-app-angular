4c5d32098778ad399b7c86ee57539390107168d4
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-03-11 20:13:18 -0400

    Create LoginFormComponent

---
+++ src/app/components/login-form/login-form.component.ts
@@ -0,0 +1,11 @@
+import { Component, output } from '@angular/core';
+
+@Component({
+  selector: 'app-login-form',
+  imports: [],
+  templateUrl: './login-form.component.html',
+  styleUrl: './login-form.component.scss',
+})
+export class LoginFormComponent {
+  registerLinkClicked = output();
+}
