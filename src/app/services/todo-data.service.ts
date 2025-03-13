9cd3f71c6c8c86a63bc30d4d6af5a51e06a539c0
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-03-13 03:51:25 -0400

    Rework TodoDataService

--- src/app/services/todo-data.service.ts
+++ src/app/services/todo-data.service.ts
@@ -5,56 +5,97 @@ import { Task, User } from '../model/todo-data.type';
   providedIn: 'root',
 })
 export class TodoDataService {
-  localStorageKey = 'todo-users';
+  usersKey = 'todo-users';
+  currentUserKey = 'todo-user-id';
 
-  createLocalStorageItem() {
-    const users = [
+  users: Array<User> = [];
+
+  constructor() {
+    this.loadUserList();
+  }
+
+  createUserList(): Array<User> {
+    const tasks: Array<Task> = [];
+    const randomBoolean = () => {
+      const random = Math.random();
+      return random < 0.5 ? true : false;
+    };
+
+    for (let i = 0; i < 10; i++) {
+      tasks.push({
+        id: i,
+        title: 'Task #' + i,
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id nulla imperdiet, pulvinar mi et, tincidunt nulla.',
+        dueDate: new Date('2025-08-13'),
+        completed: randomBoolean(),
+      });
+    }
+
+    const users: Array<User> = [
       {
         id: 0,
         name: 'John Johnson',
         email: 'jj666@mail.net',
-        tasks: [],
+        tasks,
       },
     ];
 
-    localStorage.setItem(this.localStorageKey, JSON.stringify(users));
+    localStorage.setItem(this.usersKey, JSON.stringify(users));
+    return users;
   }
 
-  setLocalStorageItem(users: Array<User>) {
-    localStorage.setItem(this.localStorageKey, JSON.stringify(users));
-  }
-
-  getAllUsers(): Array<User> {
-    if (!localStorage.getItem(this.localStorageKey)) {
-      this.createLocalStorageItem();
+  saveUserList(users?: Array<User>) {
+    if (users) {
+      localStorage.setItem(this.usersKey, JSON.stringify(users));
+      return;
     }
 
-    let json = localStorage.getItem(this.localStorageKey)!;
-    return JSON.parse(json);
+    localStorage.setItem(this.usersKey, JSON.stringify(this.users));
+  }
+
+  loadUserList() {
+    if (!localStorage.getItem(this.usersKey)) {
+      this.users = this.createUserList();
+    }
+
+    let json = localStorage.getItem(this.usersKey)!;
+    this.users = JSON.parse(json);
   }
 
   getUserByEmail(email: string) {
-    const users = this.getAllUsers();
-    return users.find((u) => u.email === email);
-  }
-
-  getTask(user: User, id: number) {
-    return user.tasks.find((t) => t.id === id);
+    return this.users.find((u) => u.email === email);
   }
 
   addUser(email: string, name: string) {
-    const users = this.getAllUsers();
-    const lastId = users.reduce((previous, current) =>
+    const lastId = this.users.reduce((previous, current) =>
       current.id > previous.id ? current : previous
     ).id;
 
-    users.push({
+    this.users.push({
       id: lastId + 1,
       email,
       name,
       tasks: [],
     });
 
-    this.setLocalStorageItem(users);
+    this.saveUserList();
+  }
+
+  setCurrentUser(user: User | number) {
+    if (typeof user === 'number') {
+      localStorage.setItem(this.currentUserKey, JSON.stringify(user));
+      return;
+    }
+
+    localStorage.setItem(this.currentUserKey, JSON.stringify(user.id));
+  }
+
+  getCurrentUser() {
+    const json = localStorage.getItem(this.currentUserKey);
+    if (!json) return null;
+
+    const id: number = JSON.parse(json);
+    return this.users.find((u) => u.id === id) || null;
   }
 }
