d608105adb0d10c3ef2ccfc7a1ac2328c82003a5
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-03-13 22:49:15 -0400

    Update TodoListComponent

--- src/app/pages/todo-list/todo-list.component.ts
+++ src/app/pages/todo-list/todo-list.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { Task } from '../../model/todo-data.type';
+import { TodoDataService } from '../../services/todo-data.service';
+import { TodoTaskComponent } from '../../components/todo-task/todo-task.component';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [],
+  imports: [HeaderComponent, TodoTaskComponent],
   templateUrl: './todo-list.component.html',
-  styleUrl: './todo-list.component.scss'
+  styleUrl: './todo-list.component.scss',
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+  tds = inject(TodoDataService);
+  tasks = signal<Array<Task>>([]);
+  userName = signal('');
 
+  ngOnInit(): void {
+    const user = this.tds.getCurrentUser();
+    if (!user) return;
+
+    this.userName.set(user.name);
+    this.tasks.set(user.tasks);
+  }
 }
