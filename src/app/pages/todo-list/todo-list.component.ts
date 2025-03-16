7275f4567d000833cc4802284ab77797e88530f4
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-03-16 04:03:45 -0400

    Add footer to TodoListComponent

--- src/app/pages/todo-list/todo-list.component.ts
+++ src/app/pages/todo-list/todo-list.component.ts
@@ -6,6 +6,7 @@ import { TodoTaskComponent } from '../../components/todo-task/todo-task.componen
 import { NewTaskFormComponent } from '../../components/new-task-form/new-task-form.component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-todo-list',
@@ -13,6 +14,7 @@ import { FormsModule } from '@angular/forms';
     HeaderComponent,
     TodoTaskComponent,
     NewTaskFormComponent,
+    FooterComponent,
     FormsModule,
   ],
   templateUrl: './todo-list.component.html',
