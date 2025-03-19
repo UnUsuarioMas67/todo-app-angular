d6891ac3e59fb2812fa674ad5367d74175c40eba
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-03-19 00:41:19 -0400

    Add highlight on task dates when they are expired

--- src/app/components/todo-task/todo-task.component.ts
+++ src/app/components/todo-task/todo-task.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, OnInit, output } from '@angular/core';
 import { Task } from '../../model/todo-data.type';
 import { DatePipe, NgClass } from '@angular/common';
 
@@ -8,8 +8,14 @@ import { DatePipe, NgClass } from '@angular/common';
   templateUrl: './todo-task.component.html',
   styleUrl: './todo-task.component.scss',
 })
-export class TodoTaskComponent {
+export class TodoTaskComponent implements OnInit {
   task = input.required<Task>();
   checkClicked = output<Task>();
   deleteClicked = output<Task>();
+  expired = false;
+
+  ngOnInit(): void {
+    this.expired =
+      new Date().getTime() > new Date(this.task().dueDate).getTime();
+  }
 }
