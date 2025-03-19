56e8313b8a42e5f2e931ca73d09f1c2d214a0337
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-03-19 03:14:41 -0400

    Create task edit UI

--- src/app/components/todo-task/todo-task.component.ts
+++ src/app/components/todo-task/todo-task.component.ts
@@ -1,15 +1,46 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, OnInit, output, signal } from '@angular/core';
 import { Task } from '../../model/todo-data.type';
-import { DatePipe, NgClass } from '@angular/common';
+import { DatePipe, formatDate, NgClass } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-task',
-  imports: [NgClass, DatePipe],
+  imports: [NgClass, DatePipe, FormsModule],
   templateUrl: './todo-task.component.html',
   styleUrl: './todo-task.component.scss',
 })
-export class TodoTaskComponent {
+export class TodoTaskComponent implements OnInit {
   task = input.required<Task>();
   checkClicked = output<Task>();
   deleteClicked = output<Task>();
+
+  editing = signal<boolean>(false);
+  editObj = {
+    title: '',
+    description: '',
+    date: '',
+    time: '',
+  };
+
+  getNewEditObj() {
+    return {
+      title: this.task().title,
+      description: this.task().description,
+      date: formatDate(this.task().dueDate, 'yyyy-MM-dd', 'en-US'),
+      time: formatDate(this.task().dueDate, 'hh:mm', 'en-US'),
+    };
+  }
+
+  ngOnInit(): void {
+    this.editObj = this.getNewEditObj();
+  }
+
+  onEditClick() {
+    this.editing.set(true);
+  }
+
+  onCancelClick() {
+    this.editing.set(false);
+    this.editObj = this.getNewEditObj();
+  }
 }
