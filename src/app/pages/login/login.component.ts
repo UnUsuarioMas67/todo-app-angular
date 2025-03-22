97999cb8f7049dbcbcebd0023787b0cd6ce1a786
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-03-21 23:41:27 -0400

    Add link to return home page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { TodoDataService } from '../../services/todo-data.service';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
