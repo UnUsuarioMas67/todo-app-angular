57fd185291e97dab8a0b9ba4723fafb84f16ae70
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-03-21 23:40:13 -0400

    Change navbar buttons
    - Replace old nav links with login and register links.
    - Add link back to home to the navbar brand.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,27 +1,13 @@
-import { Component, inject, input, OnInit, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import {
-  NgbCollapseModule,
-  NgbDropdownModule,
-} from '@ng-bootstrap/ng-bootstrap';
-import { TodoDataService } from '../../services/todo-data.service';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
-  imports: [NgbCollapseModule, NgbDropdownModule, RouterLink],
+  imports: [NgbCollapseModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent implements OnInit {
-  tds = inject(TodoDataService);
-
+export class HeaderComponent {
   isMenuCollapsed = signal(true);
-  userName = signal('--');
-
-  ngOnInit(): void {
-    const user = this.tds.getCurrentUser();
-    if (!user) return;
-
-    this.userName.set(user.name);
-  }
 }
